a32611d6dc0ee18ed3aada23b002373fdc11f580
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-06 20:01:24 +0200

    Add more WHERE usage examples

--- sql/DQL/select.sql
+++ sql/DQL/select.sql
@@ -22,3 +22,22 @@ SELECT id, first_name || ' ' || last_name full_name FROM users;
 SELECT id, email, account_balance AS "accountBalance" FROM users;
 -- @block або
 SELECT id айдішнік, email пошта, account_balance "accountBalance" FROM users;
+-- WHERE
+-- @block усі користувачі з висотою 1.75
+SELECT * FROM users
+WHERE height = 1.75;
+-- @block всі користувачі які не вказали дату народження
+SELECT * FROM users
+WHERE birthday IS NULL;
+-- @block знайти всіх чоловіків
+SELECT * FROM users
+WHERE is_male;
+-- @block знайти всіх жінок
+SELECT * FROM users
+WHERE NOT is_male;
+-- @block всі користувачі з балансом від 500 до 4000
+SELECT * FROM users
+WHERE account_balance >= 500 AND account_balance <= 4000;
+-- @block всі користувачі з балансом від 500 до 4000
+SELECT * FROM users
+WHERE account_balance BETWEEN 500 AND 4000;
