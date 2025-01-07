537ac77d045846aec1b201e88354786f77aa3f24
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-07 18:50:55 +0200

    Add single user insertion example

--- script/index.js
+++ script/index.js
@@ -10,13 +10,37 @@ const config = {
 
 const client = new Client(config);
 
+const user = {
+  firstName: 'Insert',
+  lastName: 'Insertovich',
+  email: 'inserter@mail.com',
+  accountBalance: 250,
+  height: 1.67,
+  isMale: true,
+  birthday: '1980-07-15',
+}
+
 async function startScript() {
 
   await client.connect();
 
-  const res = await client.query('SELECT * FROM users;');
+  const res = await client.query(`
+    INSERT INTO users (
+      first_name,
+      last_name,
+      email,
+      is_male,
+      height,
+      account_balance,
+      birthday
+    ) VALUES
+    ('${user.firstName}', '${user.lastName}',' ${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')
+    RETURNING *;
+  `);
   
-  console.log(res); // res.rows - резульатати запиту (користувачі)
+  // console.log(res); // res.rows - резульатати запиту (користувачі)
+
+  console.log(res.rows[0]);
   
   await client.end();
 }
