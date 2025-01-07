3a0278c1a86629d0b9a6d2a937ff58d373a3ad2f
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-07 19:02:23 +0200

    Add multiple users insertion example

--- script/index.js
+++ script/index.js
@@ -5,8 +5,8 @@ const config = {
   password: 'postgres',
   host: 'localhost',
   port: 5432,
-  database: 'pfe-2024-1_lessons'
-}
+  database: 'pfe-2024-1_lessons',
+};
 
 const client = new Client(config);
 
@@ -18,10 +18,51 @@ const user = {
   height: 1.67,
   isMale: true,
   birthday: '1980-07-15',
+};
+
+const users = [
+  {
+    firstName: 'Insert 1',
+    lastName: 'Insertovich 1',
+    email: 'inserter1@mail.com',
+    accountBalance: 552245,
+    height: 1.67,
+    isMale: false,
+    birthday: '1995-11-02',
+  },
+  {
+    firstName: 'Insert 2',
+    lastName: 'Insertovich 2',
+    email: 'inserter2@mail.com',
+    accountBalance: 0,
+    height: 1.98,
+    isMale: false,
+    birthday: '1988-04-28',
+  },
+  {
+    firstName: 'Insert 3',
+    lastName: 'Insertovich 3',
+    email: 'inserter3@mail.com',
+    accountBalance: 6500,
+    height: 1.58,
+    isMale: true,
+    birthday: '1980-03-03',
+  },
+];
+
+function mapUser(user) {
+  return `('${user.firstName}', '${user.lastName}',' ${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')`;
 }
 
-async function startScript() {
+const usersInsertStringsArray = users.map(mapUser);
 
+// console.log(usersInsertStringsArray);
+
+const usersInsertString = usersInsertStringsArray.join(',');
+
+// console.log(usersInsertString);
+
+async function startScript() {
   await client.connect();
 
   const res = await client.query(`
@@ -34,14 +75,14 @@ async function startScript() {
       account_balance,
       birthday
     ) VALUES
-    ('${user.firstName}', '${user.lastName}',' ${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')
+    ${usersInsertString}
     RETURNING *;
   `);
-  
+
   // console.log(res); // res.rows - резульатати запиту (користувачі)
 
   console.log(res.rows[0]);
-  
+
   await client.end();
 }
 
