dc8e56172b92fde43b557241e4029971df8363cd
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-13 16:06:15 +0200

    Expand database generation script

--- script/index.js
+++ script/index.js
@@ -1,34 +1,100 @@
+const fs = require('fs').promises;
 const { Client } = require('pg');
+const _ = require('lodash');
 const config = require('../configs/db.json');
+const { chanceToMakeOrder } = require('../configs/ordersGeneration.json');
 const { getUsers } = require('../api');
-const { mapUser } = require('../utils/userUtils');
+const { mapUsers } = require('../utils/userUtils');
+const { generateProducts } = require('../utils/productUtils');
+const { generateOrders, generateProductsToOrders } = require('../utils/orderUtils');
+const { generateReviews, generateRatings, generateReviewsMap } = require('../utils/reviewUtils');
 
 const client = new Client(config);
 
 async function startScript() {
   await client.connect();
 
+  const resetQuery = await fs.readFile(`${__dirname}/sql/resetDB.sql`, 'utf-8');
+
+  await client.query(resetQuery);
+  
   const users = await getUsers();
 
-  const usersInsertStringsArray = users.map(mapUser);
+  await client.query('DELETE FROM users;');
 
-  const usersInsertString = usersInsertStringsArray.join(',');
-
-  const res = await client.query(`
+  const { rows: createdUsers } = await client.query(`
     INSERT INTO users (
       first_name,
       last_name,
       email,
       is_male,
-      height,
       account_balance,
+      height,
+      weight,
+      comments,
       birthday
     ) VALUES
-    ${usersInsertString}
+    ${mapUsers(users)}
     RETURNING *;
   `);
 
-  // console.log(res); // res.rows - резульатати запиту (користувачі)
+  const { rows: createdProducts } = await client.query(`
+    INSERT INTO products (
+      "name", 
+      "category", 
+      "description",
+      "manufacturer",
+      "price",
+      "quantity"
+    )
+    VALUES
+    ${generateProducts()}
+    RETURNING *
+  ;`);
+
+  const orderingUsers = createdUsers.filter(
+    () => _.random(0, 100) <= chanceToMakeOrder
+  );
+
+  const { rows: createdOrders } = await client.query(`
+    INSERT INTO orders (
+      "user_id",
+      "status"
+    )
+    VALUES
+    ${generateOrders(orderingUsers)}
+    RETURNING *
+    ;`);
+
+  const { rows: productsToOrdersResult } = await client.query(`
+    INSERT INTO products_to_orders (
+      product_id,
+      order_id,
+      quantity
+    ) VALUES
+    ${generateProductsToOrders({
+      orders: createdOrders,
+      products: createdProducts,
+    })}
+    RETURNING *
+    ;
+  `);
+
+  const reviewsMap = generateReviewsMap(createdOrders, productsToOrdersResult);
+
+  await client.query(`
+    BEGIN;
+    
+    INSERT INTO reviews
+    (user_id, product_id, rating_id, body)
+    VALUES ${generateReviews(reviewsMap)};
+    
+    INSERT INTO ratings
+    (review_id, rating)
+    VALUES ${generateRatings(reviewsMap)};
+    
+    COMMIT;`);
+
 
   await client.end();
 }
--- utils/userUtils.js
+++ utils/userUtils.js
@@ -1,13 +1,21 @@
 const _ = require('lodash');
 const userGenConfig = require('../configs/usersGeneration.json');
 
-module.exports.mapUser = (user) => {
+const mapUser = (user) => {
 
   const { name: {first, last}, email, gender, dob: {date: birthday}} = user;
 
+  const isMale = gender === 'male';
+
   const height = _.random(userGenConfig.minHeight, userGenConfig.maxHeight);
-
   const accountBalance = _.random(userGenConfig.minAccountBalance, userGenConfig.maxAccountBalance);
+  const weight = _.random(userGenConfig.minWeight, userGenConfig.maxWeight);
+  const comments = _.random(userGenConfig.minComments, userGenConfig.maxComments);
 
-  return `('${first}', '${last}',' ${email}', ${gender === 'male'}, ${height}, ${accountBalance}, '${birthday}')`;
+  return `('${first}', '${last}', '${email}', ${isMale}, ${accountBalance}, ${height}, ${weight}, ${comments}, '${birthday}')`;
 }
+
+const mapUsers = (usersArr) => usersArr.map((user) => mapUser(user)).join(',');
+
+module.exports.mapUser = mapUser;
+module.exports.mapUsers = mapUsers;
